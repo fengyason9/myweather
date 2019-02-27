613e86d8de7d4ef2556a9829a079fa5109c2a001
rich <rich@rich.com>
2019-02-27 21:06:06 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/myweather/db/City.java
@@ -0,0 +1,42 @@
+package com.example.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provincdId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvincdId() {
+        return provincdId;
+    }
+
+    public void setProvincdId(int provincdId) {
+        this.provincdId = provincdId;
+    }
+}
